8bf4e62d0f9b2970da3f2e6fc587de36edbfcd05
Jean Vieytes <jean.vieytes.pm@gmail.com>
2026-03-11 18:44:03 -0300

    Agregando nuevas pruebas publicas

--- tests/create/createpet.spec.ts
+++ tests/create/createpet.spec.ts
@@ -1,4 +1,4 @@
-import test from "@playwright/test"
+import test, { expect } from "@playwright/test"
 import Logger from "../../utils/Logger"
 import { CreateNewPet } from "../../tasks/create/createNewPet"
 import { CheckPetWasCreated } from "../../tasks/create/checkPetWasCreated"
@@ -15,35 +15,35 @@ import { faker } from "@faker-js/faker"
 
 test('Crear una nueva mascota', async ({ request }) => {
 
-  const newPetRequest:NewPetRequestModel = {
-    name: faker.animal.cat(),
-    type: "Perro",
-    age: 1
-  }
+    const newPetRequest: NewPetRequestModel = {
+        name: faker.animal.cat(),
+        type: "Perro",
+        age: 1
+    }
 
-  Logger.info("Creando una nueva mascota")
+    Logger.info("Creando una nueva mascota")
 
-  const createNewPet = new CreateNewPet(request)
-  const newPetResponse = await createNewPet.withInfo(newPetRequest)
-  const checkPetWasCreated = new CheckPetWasCreated(newPetRequest)
-  await checkPetWasCreated.withInfo(newPetResponse)
+    const createNewPet = new CreateNewPet(request)
+    const newPetResponse = await createNewPet.withInfo(newPetRequest)
+    const checkPetWasCreated = new CheckPetWasCreated(newPetRequest)
+    await checkPetWasCreated.withInfo(newPetResponse)
 
 })
 
 test('Crear una nueva mascota2', async ({ request }) => {
 
-  const newPetRequest:NewPetRequestModel = {
-    name: faker.animal.cat(),
-    type: "Perro",
-    age: 1
-  }
+    const newPetRequest: NewPetRequestModel = {
+        name: faker.animal.cat(),
+        type: "Perro",
+        age: 1
+    }
 
-  Logger.info("Creando una nueva mascota")
+    Logger.info("Creando una nueva mascota")
 
-  const createNewPet = new CreateNewPet(request)
-  const newPetResponse = await createNewPet.withInfo(newPetRequest)
-  const checkPetWasCreated = new CheckPetWasCreated(newPetRequest)
-  await checkPetWasCreated.withInfo(newPetResponse)
+    const createNewPet = new CreateNewPet(request)
+    const newPetResponse = await createNewPet.withInfo(newPetRequest)
+    const checkPetWasCreated = new CheckPetWasCreated(newPetRequest)
+    await checkPetWasCreated.withInfo(newPetResponse)
 
 })
 
@@ -54,3 +54,31 @@ test('Crear una nueva mascota2', async ({ request }) => {
 // test.afterEach(async() =>{
 //     Logger.info('Running After Tests')
 // })
+
+test('Crear una nueva mascota - Public', { tag: '@prod' }, async ({ request }) => {
+    const createPetResponse = await request.post('https://petstore.swagger.io/v2/pet', {
+        data: {
+            "id": 0,
+            "category": {
+                "id": 0,
+                "name": "string"
+            },
+            "name": "doggie",
+            "photoUrls": [
+                "string"
+            ],
+            "tags": [
+                {
+                    "id": 0,
+                    "name": "string"
+                }
+            ],
+            "status": "available"
+        }
+    })
+
+    expect(createPetResponse.status()).toBe(200)
+    const createPetResponseJson = await createPetResponse.json()
+    
+    expect(createPetResponseJson.id).toBeTruthy()
+})
